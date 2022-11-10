90d5b26ba6b05fa530216f272de591396e740621
mahdia <mahdiaibrahimi@tekschool.us>
2022-11-10 12:23:21 -0500

    this is test

---
+++ src/gitprojectpractice/gitProjectpractice.java
@@ -0,0 +1,12 @@
+package gitprojectpractice;
+
+public class gitProjectpractice {
+
+	public static void main(String[] args) {
+		
+
+	}
+public void test() {
+	System.out.println("this is from local repository");
+}
+}
